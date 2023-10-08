842bad073591dc7158f461001815791d10cd8e67
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-08 22:52:40 +0530

    JPA class to handle request for data

---
+++ src/main/java/dev/archimedes/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package dev.archimedes.repository;
+
+import dev.archimedes.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
