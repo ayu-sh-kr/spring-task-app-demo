df4d0fc66f8912175f35ddc4c60c888ea7f7ca27
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-10-08 22:16:26 +0530

    created an entity Event.java

--- src/main/java/dev/archimedes/models/Event.java
+++ src/main/java/dev/archimedes/models/Event.java
@@ -1,11 +1,26 @@
 package dev.archimedes.models;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity(name = "Event")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String event_name;
     private String event_description;
     private LocalDateTime event_start;
     private LocalDateTime event_end;
+
+    @ManyToOne
+    @JoinColumn(name = "user_user_id")
     private User user;
 }
